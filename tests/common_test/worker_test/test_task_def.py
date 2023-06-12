79045d11e6c31e2de31142b9f638a7efb1e076e1
Martin Revaj <mrevaj@frinx.io>
2023-06-12 12:50:19 +0200

    tests added - use pytest instead of unittest - old code moved to conftest to keep clean repo - call tests in pre-check CI - fix problem with pythonpath to run tests

---
+++ tests/common_test/worker_test/test_task_def.py
@@ -0,0 +1,149 @@
+from typing import Any
+from typing import Optional
+
+from frinx.common.frinx_rest import X_FROM
+from frinx.common.worker.service import ServiceWorkersImpl
+from frinx.common.worker.task import Task
+from frinx.common.worker.task_def import TaskDefinition
+from frinx.common.worker.task_def import DefaultTaskDefinition
+from frinx.common.worker.task_def import TaskInput
+from frinx.common.worker.task_def import TaskOutput
+from frinx.common.worker.task_result import TaskResult
+from frinx.common.conductor_enums import TaskResultStatus
+from frinx.common.worker.worker import WorkerImpl
+from pydantic import Field
+
+
+class TestTaskGenerator:
+    def test_create_task_def(self) -> None:
+        class Http(ServiceWorkersImpl):
+            class HttpTask1(WorkerImpl):
+                class WorkerDefinition(TaskDefinition):
+                    name = "HTTP_task_1"
+                    description = "Generic http task"
+                    labels = ["BASIC", "HTTP"]
+                    timeout_seconds = 360
+                    response_timeout_seconds = 360
+                    execution_name_space = "execution_namespace"
+                    retry_count = 10
+                    concurrent_exec_limit = 5
+
+                class WorkerInput(TaskInput):
+                    http_request: Optional[str | dict[str, Any]]
+
+                class WorkerOutput(TaskOutput):
+                    response: Any
+                    body: Any
+                    status_code: int = Field(..., alias="statusCode")
+                    cookies: dict[str, Any]
+
+                def execute(self, task_def: Task) -> TaskResult:
+                    return TaskResult(status=TaskResultStatus.COMPLETED)
+
+            class HttpTask2(WorkerImpl):
+                class WorkerDefinition(TaskDefinition):
+                    name = "HTTP_task"
+                    description = "Generic http task"
+                    labels = ["BASIC", "HTTP"]
+                    timeout_seconds = 360
+                    response_timeout_seconds = 360
+
+                class WorkerInput(TaskInput):
+                    http_request: Optional[str | dict[str, Any]]
+
+                class WorkerOutput(TaskOutput):
+                    response: Any
+                    body: Any
+                    status_code: int = Field(..., alias="statusCode")
+                    cookies: dict[str, Any]
+
+                def execute(self, task_def: Task) -> TaskResult:
+                    return TaskResult(status=TaskResultStatus.COMPLETED)
+
+        tasks = Http().tasks()
+        test_task = []
+        for task in tasks:
+            test_task.append(task.task_def.dict(exclude_none=True, by_alias=True))
+
+        test_mock = [
+            {
+                "name": "HTTP_task_1",
+                "description": '{"description": "Generic http task", "labels": ["BASIC", "HTTP"]}',
+                "retryCount": 10,
+                "timeoutSeconds": 360,
+                "inputKeys": ["http_request"],
+                "outputKeys": ["response", "body", "statusCode", "cookies"],
+                "timeoutPolicy": "ALERT_ONLY",
+                "retryLogic": "FIXED",
+                "retryDelaySeconds": 0,
+                "responseTimeoutSeconds": 360,
+                "concurrentExecLimit": 5,
+                "rateLimitPerFrequency": 0,
+                "rateLimitFrequencyInSeconds": 5,
+                "executionNameSpace": "execution_namespace",
+                "ownerEmail": "fm-base-workers",
+            },
+            {
+                "name": "HTTP_task",
+                "description": '{"description": "Generic http task", "labels": ["BASIC", "HTTP"]}',
+                "retryCount": 0,
+                "timeoutSeconds": 360,
+                "inputKeys": ["http_request"],
+                "outputKeys": ["response", "body", "statusCode", "cookies"],
+                "timeoutPolicy": "ALERT_ONLY",
+                "retryLogic": "FIXED",
+                "retryDelaySeconds": 0,
+                "responseTimeoutSeconds": 360,
+                "rateLimitPerFrequency": 0,
+                "rateLimitFrequencyInSeconds": 5,
+                "ownerEmail": "fm-base-workers",
+            },
+        ]
+
+        assert test_mock == test_task
+
+    def test_create_task_def_with_custom_default(self) -> None:
+        class HttpTask(WorkerImpl):
+            class WorkerDefinition(TaskDefinition):
+                name = "HTTP_task"
+                description = "Generic http task"
+                labels = ["BASIC", "HTTP"]
+                timeout_seconds = 360
+                response_timeout_seconds = 360
+
+            class WorkerInput(TaskInput):
+                http_request: Optional[str | dict[str, Any]]
+
+            class WorkerOutput(TaskOutput):
+                response: Any
+                body: Any
+                status_code: int = Field(..., alias="statusCode")
+                cookies: dict[str, Any]
+
+            def execute(self, task: Task) -> TaskResult:
+                return TaskResult(status=TaskResultStatus.COMPLETED)
+
+        class DefaultCustomTaskDefinition(DefaultTaskDefinition):
+            limit_to_thread_count: int = 10
+
+        test_task = HttpTask(task_def_template=DefaultCustomTaskDefinition).task_def.dict(
+            exclude_none=True
+        )
+        test_mock = {
+            "name": "HTTP_task",
+            "description": '{"description": "Generic http task", "labels": ["BASIC", "HTTP"]}',
+            "retry_count": 0,
+            "timeout_seconds": 360,
+            "input_keys": ["http_request"],
+            "output_keys": ["response", "body", "statusCode", "cookies"],
+            "timeout_policy": "ALERT_ONLY",
+            "retry_logic": "FIXED",
+            "retry_delay_seconds": 0,
+            "response_timeout_seconds": 360,
+            "rate_limit_per_frequency": 0,
+            "rate_limit_frequency_in_seconds": 5,
+            "owner_email": X_FROM,
+            "limit_to_thread_count": 10,
+        }
+
+        assert test_mock == test_task
---
+++ tests/common_test/workflow_test/test_workflow_generator.py
@@ -0,0 +1,137 @@
+from frinx.common.conductor_enums import TimeoutPolicy
+from frinx.common.workflow.task import SimpleTask
+from frinx.common.workflow.task import SimpleTaskInputParameters
+from frinx.common.workflow.workflow import FrontendWFInputFieldType
+from frinx.common.workflow.workflow import WorkflowImpl
+from frinx.common.workflow.workflow import WorkflowInputField
+from tests.conftest import Http as HttpWorker
+
+
+class TestWorkflowGenerator:
+    def test_workflow_build(self) -> None:
+        class HttpRequest(WorkflowImpl):
+            name = "Http_request"
+            version = 1
+            description = "Simple HTTP request"
+            labels = ["HTTP"]
+            rbac = ["network-admin"]
+            timeout_policy = TimeoutPolicy.TIME_OUT_WORKFLOW
+            restartable = True
+
+            class WorkflowInput(WorkflowImpl.WorkflowInput):
+                uri = WorkflowInputField(
+                    name="uri",
+                    frontend_default_value="",
+                    description="Request url",
+                    type=FrontendWFInputFieldType.STRING,
+                )
+
+                contentType = WorkflowInputField(
+                    name="contentType",
+                    frontend_default_value="application/json",
+                    description="Request contentType header",
+                    type=FrontendWFInputFieldType.STRING,
+                )
+
+                method = WorkflowInputField(
+                    name="method",
+                    frontend_default_value="GET",
+                    description="Request method",
+                    options=["GET", "PUT", "POST", "DELETE", "PATCH"],
+                    type=FrontendWFInputFieldType.SELECT,
+                )
+
+                headers = WorkflowInputField(
+                    name="headers",
+                    frontend_default_value={},
+                    description="Request headers",
+                    type=FrontendWFInputFieldType.TEXTAREA,
+                )
+
+                body = WorkflowInputField(
+                    name="body",
+                    frontend_default_value={},
+                    description="Request body",
+                    type=FrontendWFInputFieldType.TEXTAREA,
+                )
+
+                timeout = WorkflowInputField(
+                    name="timeout",
+                    frontend_default_value=360,
+                    description="Request timeout",
+                    type=FrontendWFInputFieldType.INT,
+                )
+
+            class WorkflowOutput(WorkflowImpl.WorkflowOutput):
+                data: str
+
+            def workflow_builder(self, workflow_inputs: WorkflowInput) -> None:
+                http_request = {
+                    "uri": workflow_inputs.uri.wf_input,
+                    "contentType": workflow_inputs.contentType.wf_input,
+                    "method": workflow_inputs.method.wf_input,
+                    "headers": workflow_inputs.headers.wf_input,
+                    "body": workflow_inputs.body.wf_input,
+                }
+
+                self.tasks.append(
+                    SimpleTask(
+                        name=HttpWorker.HttpTask,
+                        task_reference_name="http_task",
+                        input_parameters=SimpleTaskInputParameters(
+                            http_request=http_request
+                        ),
+                    )
+                )
+
+        test_workflow = HttpRequest().dict(exclude_none=True)
+
+        test_mock = {
+            "name": "Http_request",
+            "version": 1,
+            "description": '{"description": "Simple HTTP request", "labels": ["HTTP"], "rbac": ["network-admin"]}',
+            "labels": ["HTTP"],
+            "rbac": ["network-admin"],
+            "restartable": True,
+            "output_parameters": {},
+            "input_parameters": [
+                '{"uri": {"value": "", "description": "Request url", "type": "string", "options": null}}',
+                '{"contentType": {"value": "application/json", "description": "Request contentType header", "type": "string", "options": null}}',
+                '{"method": {"value": "GET", "description": "Request method", "type": "select", "options": ["GET", "PUT", "POST", "DELETE", "PATCH"]}}',
+                '{"headers": {"value": {}, "description": "Request headers", "type": "textarea", "options": null}}',
+                '{"body": {"value": {}, "description": "Request body", "type": "textarea", "options": null}}',
+                '{"timeout": {"value": 360, "description": "Request timeout", "type": "int", "options": null}}',
+            ],
+            "tasks": [
+                {
+                    "name": "HTTP_task",
+                    "task_reference_name": "http_task",
+                    "type": "SIMPLE",
+                    "start_delay": 0,
+                    "optional": False,
+                    "async_complete": False,
+                    "default_case": [],
+                    "input_parameters": {
+                        "http_request": {
+                            "uri": "${workflow.input.uri}",
+                            "contentType": "${workflow.input.contentType}",
+                            "method": "${workflow.input.method}",
+                            "headers": "${workflow.input.headers}",
+                            "body": "${workflow.input.body}",
+                        }
+                    },
+                }
+            ],
+            "timeout_policy": "TIME_OUT_WF",
+            "timeout_seconds": 60,
+            "input_template": {
+                "uri": "",
+                "contentType": "application/json",
+                "method": "GET",
+                "headers": {},
+                "body": {},
+                "timeout": 360,
+            },
+        }
+
+        assert test_mock == test_workflow
