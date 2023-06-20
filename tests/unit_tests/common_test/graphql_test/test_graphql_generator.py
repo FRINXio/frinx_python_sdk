071c5ea393abf48a4c2f03baac78c3d70dee83a7
Jozef Volak <jvolak@frinx.io>
2023-06-20 15:49:06 +0200

    Graphql schema to pydantic model generator  - convert GraphQL schema to Pydantic model  - convert Pydantic model to GraphQL request  - simple graphql client

---
+++ tests/unit_tests/common_test/graphql_test/test_graphql_generator.py
@@ -0,0 +1,58 @@
+import json
+
+from frinx.common.graphql.schema_converter import GraphqlJsonParser
+
+
+class TestTaskGenerator:
+    def test_render_json(self) -> None:
+        reference_file = 'tests/unit_tests/common_test/graphql_test/model.py'
+        json_schema = 'tests/unit_tests/common_test/graphql_test/schema.json'
+        reference = open(reference_file).read()
+        schema = json.loads(open(json_schema).read())
+        converted = GraphqlJsonParser(input_json=schema).render()
+        assert reference == converted
+
+    def test_render_query(self) -> None:
+        from model import Blueprint
+        from model import BlueprintConnection
+        from model import BlueprintEdge
+        from model import BlueprintsQuery
+
+        reference = '{ blueprints { edges { cursor node { createdAt name template updatedAt } } totalCount } }'
+        query = BlueprintsQuery(
+            payload=BlueprintConnection(
+                edges=BlueprintEdge(
+                    node=Blueprint(
+                        name=True,
+                        id=False
+                    )
+                )
+            )
+        ).render()
+        assert reference == query
+
+    def test_render_mutation(self) -> None:
+        from model import AddBlueprintInput
+        from model import AddBlueprintMutation
+        from model import AddBlueprintPayload
+        from model import Blueprint
+
+        bp_inputs = 'name: "IOS", template: "{ "cli": { "cli-topology:host": "sample-topology" } }"'
+        bp_payload = 'blueprint { createdAt name template updatedAt }'
+        reference = f'mutation {{ addBlueprint ( input: {{ {bp_inputs} }}) {{ { bp_payload } }} }}'
+        mutation = AddBlueprintMutation(
+            input=AddBlueprintInput(
+                name='IOS',
+                template='{ "cli": { "cli-topology:host": "sample-topology" } }'
+            ),
+            payload=AddBlueprintPayload(
+                blueprint=Blueprint(
+                    createdAt=True,
+                    id=False,
+                    name=True,
+                    template=True,
+                    updatedAt=True
+                )
+            )
+        ).render()
+        assert reference == mutation
